04c78f04d42a9c0db489c64e03efad373ca1ab7e
juan diego <juandiego89@hotmail.com>
2021-04-12 16:57:55 +0900

    add libraries for a better support or polyfills

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
+// ECMA Script Polyfills:
+import "core-js/stable";
+// Needed for the generator functions which are transpiled from your async await keywords
+import "regenerator-runtime/runtime";
 
 const store = createStore(function (state, action) {
   const _state =
